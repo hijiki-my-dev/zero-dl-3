30903ec43c94bf13fb6f0004d5dffeb4de7ec0ba
Hijiki <hijikiblog01@gmail.com>
2025-09-02 00:07:25 +0900

    step45

--- dezero/models.py
+++ dezero/models.py
@@ -17,11 +17,11 @@ class MLP(Model):
         self.layers = []
 
         for i, out_size in enumerate(fc_output_sizes):
-            layer = L.Layer(out_size)
+            layer = L.Linear(out_size)
             setattr(self, "l" + str(i), layer)
             self.layers.append(layer)
 
     def forward(self, x):
         for l in self.layers[:-1]:
             x = self.activation(l(x))
-        return self.layers[:-1](x)
+        return self.layers[-1](x)
