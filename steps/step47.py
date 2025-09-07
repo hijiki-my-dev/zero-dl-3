a70071b080922f243429d4046f2a99a94619b01e
Hijiki <hijikiblog01@gmail.com>
2025-09-07 13:13:27 +0900

    step47

---
+++ steps/step47.py
@@ -0,0 +1,24 @@
+if '__file__' in globals():
+    import os, sys
+    sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
+import numpy as np
+
+from dezero import Variable, Model, optimizers
+import dezero.layers as L
+import dezero.functions as F
+from dezero.models import MLP
+
+
+model = MLP((10, 3))
+
+x = np.array([[0.2, -0.4], [0.3, 0.5], [1.3, -3.2], [2.1, 0.3]])
+t = np.array([2, 0, 1, 0])
+
+y = model(x)
+p = F.softmax_simple(y)
+print(y)
+print(p)
+
+loss = F.softmax_cross_entropy_simple(y, t)
+loss.backward()
+print(loss)
